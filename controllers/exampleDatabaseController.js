8df974ff0362763bcc0fc26dfa98e3e59448e5ef
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2019-10-14 11:04:04 -0700

    Setup for local DB connections

---
+++ controllers/exampleDatabaseController.js
@@ -0,0 +1,53 @@
+// ======================================
+// ======================================
+// This controller manipulates a single object using RESTful routes.
+// These routes include error catching and Object based returns.
+// ======================================
+// ======================================
+
+const express = require('express');
+const exampleObjectRouter = express.Router();
+const errorWrapper = require('./errorWrapper.js');
+const database = require('../db/Database.js');
+const moment = require('moment');
+
+// ======================================
+// Get all Examples.
+// ROUTE: GET `/api/examples/`
+exampleObjectRouter.get('/', errorWrapper(async (req, res, next) => {
+  // Knex database query.
+  const readResults = await database('examples')
+    .select('*')
+    .orderBy('exampleId', 'asc')
+    .catch((err) => { throw new Error(err) });
+
+    // Return HTTP status and JSON results object.
+    return res.status(200).json({
+      success: true,
+      message: 'API returned list of all Examples',
+      results: readResults
+    });
+}));
+
+// ======================================
+// Get individual Example.
+// ROUTE: GET `api/examples/:exampleId`
+exampleObjectRouter.get('/:exampleId', errorWrapper(async (req, res, next) => {
+  // Throw Error if body contains exampleId.
+  if (req.body.exampleId) { throw new Error('Request body cannot contain exampleId') };
+
+  // "Database" query.
+  const readResults = helloWorld[req.params.exampleId]
+  // Throw error if Example does not exist.
+  if (!readResults) { throw new Error("Example with provided exampleId does not exist") };
+
+  // Return HTTP status and JSON results object.
+  return res.status(200).json({
+    success: true,
+    message: 'API returned Example with exampleId of ' + req.params.exampleId,
+    results: readResults
+  });
+}));
+
+// ======================================
+module.exports = exampleObjectRouter;
---
+++ db/Database.js
@@ -0,0 +1,45 @@
+const pg = require('pg');
+
+const config = require('../knexfile.js');
+const database = require('knex')(config[process.env.NODE_ENV] || config['development']);
+
+const knexMigrations = async (production_env) => {
+  console.log('>>> Running `knex migrate:latest`... <<<')
+  if (production_env == true) {
+    const migrationResults = await database.migrate.latest('production')
+    console.log(migrationResults)
+  } else {
+    const migrationResults = await database.migrate.latest('development')
+    console.log(migrationResults)
+  }
+  console.log('>>> Migrations Complete. <<<')
+};
+
+const knexSeeds = async (production_env) => {
+  console.log('>>> Running `knex seed:run`... <<<')
+  if (production_env == true) {
+    await database.seed.run('production')
+  } else {
+    await database.seed.run('development')
+  }
+  console.log('>>> Seed Complete. <<<')
+};
+
+// On API startup, this will run knex migrate:latest to ensure that our DB is up to date.
+const knexSetup = async () => {
+  if (process.env.NODE_ENV != 'development') {
+    console.log('>>> Production Environment Detected <<<')
+    pg.defaults.ssl = true;
+    await knexMigrations(true)
+    await knexSeeds(true)
+  } else {
+    console.log('>>> Development Environment Detected <<<')
+    await knexMigrations(false)
+    await knexSeeds(false)
+  }
+  console.log('>>> Knex Setup Completed <<<')
+}
+
+knexSetup();
+
+module.exports = database;
